a5d05776c17b5169384e2fadda04b16a4a829cfb
Oleg Kolosov <bazurbat@gmail.com>
2016-04-28 17:51:51 +0300

    Improve Find smcore and smhw modules

--- FindSmCore.cmake
+++ FindSmCore.cmake
@@ -1,10 +1,14 @@
 include(FindPackageHandleStandardArgs)
 
-find_path(SMCORE_INCLUDE_DIRS smcore.h
-    PATH_SUFFIXES smcore)
-find_library(SMCORE_LIBRARIES smcore)
+find_path(SMCORE_INCLUDE_DIR smcore/smcore.h)
+find_library(SMCORE_LIBRARY smcore)
 
 mark_as_advanced(SMCORE_INCLUDE_DIRS SMCORE_LIBRARIES)
+
+set(SMCORE_INCLUDE_DIRS
+    ${SMCORE_INCLUDE_DIR})
+set(SMCORE_LIBRARIES
+    ${SMCORE_LIBRARY})
 
 find_package_handle_standard_args(SMCORE DEFAULT_MSG
     SMCORE_LIBRARIES SMCORE_INCLUDE_DIRS)
--- FindSmHw.cmake
+++ FindSmHw.cmake
@@ -1,10 +1,16 @@
 include(FindPackageHandleStandardArgs)
 
-find_path(SMHW_INCLUDE_DIRS smhw.h
+find_path(SMHW_INCLUDE_DIR board_id.h
     PATH_SUFFIXES smhw)
-find_library(SMHW_LIBRARIES smhw)
+find_library(SMHW_LIBRARY smhw)
 
-mark_as_advanced(SMHW_INCLUDE_DIRS SMHW_LIBRARIES)
+mark_as_advanced(SMHW_INCLUDE_DIR SMHW_LIBRARY)
+
+set(SMHW_INCLUDE_DIRS
+    ${SMHW_INCLUDE_DIR}/hisi
+    ${SMHW_INCLUDE_DIR})
+set(SMHW_LIBRARIES
+    ${SMHW_LIBRARY})
 
 find_package_handle_standard_args(SMHW DEFAULT_MSG
     SMHW_LIBRARIES SMHW_INCLUDE_DIRS)
