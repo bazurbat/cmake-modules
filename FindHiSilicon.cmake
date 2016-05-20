fd2f4279b2480221cac7a4e9440e3451ddd69c09
Oleg Kolosov <bazurbat@gmail.com>
2016-05-20 17:13:57 +0300

    FindHiSilicon: made HiGo into component

--- FindHiSilicon.cmake
+++ FindHiSilicon.cmake
@@ -24,41 +24,48 @@ else()
     set(_hi_libraries
         hi_common
         hi_msp
-        hi_mce
-        higo
-        higoadp
-        )
+        hi_mce)
 endif()
 
 find_package_handle_standard_args(HiSilicon DEFAULT_MSG
     HISILICON_SDK_DIR)
 
-if(HISILICON_FOUND)
-    set(HISILICON_INCLUDE_DIRS ${HISILICON_SDK_DIR}/pub/include)
-    set(HISILICON_MSP_INCLUDE_DIRS ${HISILICON_SDK_DIR}/source/msp/include)
-    set(HISILICON_MSP_API_INCLUDE_DIRS ${HISILICON_SDK_DIR}/source/msp/api/include)
-    set(HISILICON_MSP_DRV_INCLUDE_DIRS ${HISILICON_SDK_DIR}/source/msp/drv/include)
-    if(ANDROID)
-        set(HISILICON_LIBRARIES "")
-    else()
-        set(HISILICON_LIBRARIES m rt pthread ${CMAKE_DL_LIBS})
-    endif()
-
-    foreach(name ${_hi_libraries})
-        set(_var _HISILICON_LIBRARY_${name})
-        if(ANDROID)
-            find_library(${_var} ${name}
-                PATHS ${HISILICON_OBJ_DIR}/SHARED_LIBRARIES/lib${name}_intermediates/LINKED
-                      ${HISILICON_OBJ_DIR}/STATIC_LIBRARIES/lib${name}_intermediates
-                NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
-        else()
-            find_library(${_var} ${name}
-                PATHS ${HISILICON_SDK_DIR}/pub/lib/share
-                      ${HISILICON_SDK_DIR}/pub/lib/static
-                      NO_DEFAULT_PATH)
-        endif()
-        if(${_var})
-            list(APPEND HISILICON_LIBRARIES ${${_var}})
-        endif()
-    endforeach()
+if(NOT HISILICON_FOUND)
+    return()
 endif()
+
+set(HISILICON_INCLUDE_DIRS ${HISILICON_SDK_DIR}/pub/include)
+set(HISILICON_MSP_INCLUDE_DIRS ${HISILICON_SDK_DIR}/source/msp/include)
+set(HISILICON_MSP_API_INCLUDE_DIRS ${HISILICON_SDK_DIR}/source/msp/api/include)
+set(HISILICON_MSP_DRV_INCLUDE_DIRS ${HISILICON_SDK_DIR}/source/msp/drv/include)
+
+if(ANDROID)
+    set(HISILICON_LIBRARIES "")
+else()
+    set(HISILICON_LIBRARIES m rt pthread ${CMAKE_DL_LIBS})
+endif()
+
+list(FIND HiSilicon_FIND_COMPONENTS HiGo HiGo_REQUIRED)
+if(HiGo_REQUIRED GREATER -1)
+    list(APPEND _hi_libraries higo higoadp)
+endif()
+
+foreach(name ${_hi_libraries})
+    set(_var _HISILICON_LIBRARY_${name})
+    if(ANDROID)
+        find_library(${_var} ${name}
+            PATHS ${HISILICON_OBJ_DIR}/SHARED_LIBRARIES/lib${name}_intermediates/LINKED
+            ${HISILICON_OBJ_DIR}/STATIC_LIBRARIES/lib${name}_intermediates
+            NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
+    else()
+        find_library(${_var} ${name}
+            PATHS ${HISILICON_SDK_DIR}/pub/lib/share
+            ${HISILICON_SDK_DIR}/pub/lib/static
+            NO_DEFAULT_PATH)
+    endif()
+    if(${_var})
+        list(APPEND HISILICON_LIBRARIES ${${_var}})
+    endif()
+endforeach()
+
+unset(_hi_libraries)
